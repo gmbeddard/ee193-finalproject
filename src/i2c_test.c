25693d83b165056ce2d37c19d8a476a688a86dda
Linda <zhlinda3@gmail.com>
2024-03-10 17:51:26 -0400

    testing i2c expressif

---
+++ src/i2c_test.c
@@ -0,0 +1,31 @@
+#include "FreeRTOSConfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h" // Used for timer delay
+#include "driver/i2c_master.h"
+#define DATA_LENGTH 100
+#define SENSOR_ADDR 0x18
+
+void app_main()
+{
+    i2c_master_bus_config_t i2c_mst_config = {
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .i2c_port = I2C_PORT_NUM_0,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .glitch_ignore_cnt = 7,
+    };
+    i2c_master_bus_handle_t bus_handle;
+
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+
+    i2c_device_config_t dev_cfg = {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = SENSOR_ADDR,
+        .scl_speed_hz = 100000,
+    };
+
+    i2c_master_dev_handle_t dev_handle;
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
+
+    i2c_master_receive(dev_handle, data_rd, DATA_LENGTH, -1);
+}
