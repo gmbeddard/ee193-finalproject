21ec8cb849545a4b8ca852dd7589464c3799304c
Emily Stanisha <emily.stanisha.17@cnu.edu>
2024-05-06 18:07:33 -0400

    added IO4 as VCC for temp IC

--- src/i2c_util.c
+++ src/i2c_util.c
@@ -18,6 +18,7 @@
 #define ACK_CHECK_DIS 0x0          // I2C master will not check ack from slave
 #define ACK_VAL 0x0                // I2C ack value
 #define NACK_VAL 0x1               // I2C nack value
+#define GPIO_OUTPUT_IO_4 4 // Example GPIO pin number
 
 esp_err_t i2c_master_init(void)
 {
@@ -51,6 +52,28 @@ esp_err_t mcp9808_read_temperature(float *temperature)
     esp_err_t ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd);
 
+    // Set IO4 high to power temp IC
+    esp_rom_gpio_pad_select_gpio(GPIO_OUTPUT_IO_4);
+    gpio_set_direction(GPIO_OUTPUT_IO_4, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_OUTPUT_IO_4, 1);
+
+    printf("Attempting I2C");
+        printf("Read raw I2C temperature: 0x%02x 0x%02x\n", data[0], data[1]); // Debugging
+
+        // Convert the data to a temperature
+        int16_t temp = data[0] << 8 | data[1];
+
+        // Mask off the flag bits
+        temp = temp & 0x1FFF;
+        if (data[0] & 0x10)
+        {                   // Check if the temperature is negative
+            temp |= 0xE000; // Set the sign bits if negative
+        }
+        *temperature = temp * 0.125;
+        printf("I2C Temperature: %.2f°C\n", *temperature);
+        // ESP_LOGI("MCP9808", "Temperature: %.2f°C", temperature);
+
+/*
     if (ret == ESP_OK)
     {
         printf("Attempting I2C");
@@ -73,7 +96,7 @@ esp_err_t mcp9808_read_temperature(float *temperature)
     {
         printf("Failed to read temperature\n");
         // ESP_LOGE("MCP9808", "Failed to read temperature");
-    }
+    }*/
 
     return ret;
 }
